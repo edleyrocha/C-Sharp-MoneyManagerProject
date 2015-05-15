02c209fc657e01e74095485dc63e72d1cf7da870
Edley Allan Hoyler <edleyrocha@hotmail.com>
2015-05-14 22:48:22 -0400

    Start Project Aplication
    //------------------------------------------------------------------------------
    //
    // <git hub="https://github.com/edleyrocha">
    //      GitHub Repositories
    // </git>
    //
    // <mail address ="edleyrocha@hotmail.com">
    //      Developer Email.
    // </mail>
    //
    
    //------------------------------------------------------------------------------

---
+++ Application/SolutionMoneyManager/MoneyManagerDesktop/Program.cs
@@ -0,0 +1,33 @@
+﻿//------------------------------------------------------------------------------
+//
+// <git hub="https://github.com/edleyrocha">
+//      GitHub Repositories
+// </git>
+//
+// <mail address ="edleyrocha@hotmail.com">
+//      Developer Email.
+// </mail>
+//
+//------------------------------------------------------------------------------
+namespace MoneyManagerDesktop
+{
+    #region ---> (Using)
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows.Forms;
+    #endregion
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+        }
+    }
+}
---
+++ Application/SolutionMoneyManager/MoneyManagerDesktop/Properties/AssemblyInfo.cs
@@ -0,0 +1,48 @@
+﻿//------------------------------------------------------------------------------
+//
+// <git hub="https://github.com/edleyrocha">
+//      GitHub Repositories
+// </git>
+//
+// <mail address ="edleyrocha@hotmail.com">
+//      Developer Email.
+// </mail>
+//
+//------------------------------------------------------------------------------
+#region ---> (Using)
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+#endregion
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MoneyManagerDesktop")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MoneyManagerDesktop")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a1f72e18-f4fc-469b-ab54-1b1901ea9aa7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
